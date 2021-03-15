4512dfa501ff6b9db459f44678bbe24b0258c65c
Jack Walker <74609541+notpengy@users.noreply.github.com>
2021-03-15 11:57:49 +0000

    Update 15/03/2021
    Init function in score.py now functional.
    Model now uploading and returning an endpoint.
    TODO: Correct dataset usage so that endpoint will return predictions.

--- score.py
+++ score.py
@@ -1,20 +1,24 @@
-from azureml.core import Model
+import json
 import joblib
+import os
+import pandas as pd
 
 
 def init():
     global model
-    model_path = Model.get_model_path("models/knn")
+
+    model_path = os.path.join(os.getenv("AZUREML_MODEL_DIR"), "knn.pkl")
     model = joblib.load(model_path)
 
 
-def run(data):
+def run(raw_data):
     try:
-        data.drop('TrackNum', 1, inplace = True)
+        data = pd.read_csv(raw_data)
+        data.drop("TrackNum", 1, inplace=True)
         raw_data = data.iloc[:, 1:5].values
         labels = data.iloc[:, 0].values
         prediction = model.predict(raw_data)
-        return prediction
+        return json.dumps(prediction.toList())
 
     except Exception as ex:
         error = str(ex)
