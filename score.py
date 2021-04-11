335c7e4a850ca765f5803c1079e225a0beacfbdd
Jack Walker <74609541+JackW-Dev@users.noreply.github.com>
2021-04-11 15:57:52 +0100

    Update 11/04/2021
    Implemented local system that prompts user for track details and predicts for both models using the data.
    TODO: Apply the implementation techniques used for the local system to correct the scoring script.

--- score.py
+++ score.py
@@ -1,27 +1,18 @@
 import json
 import joblib
 import os
-import pandas as pd
+import numpy as np
 
 
 def init():
     global model
-
     model_path = os.path.join(os.getenv("AZUREML_MODEL_DIR"), "knn.pkl")
     model = joblib.load(model_path)
 
 
-def run(data):
-    try:
-        data = pd.DataFrame(json.loads(data))
-        labels = data.iloc[:, 0].values
-        data = data.iloc[:, 1:].values
-        # print(data[0])
-        # print(labels[0])
-        # print(data.iloc[0, :])
-        prediction = model.predict(data)
-        return json.dumps(prediction.toList())
-
-    except Exception as ex:
-        error = str(ex)
-        return error
+def run(raw_data):
+    return raw_data
+    # data = json.loads(raw_data)
+    # data = np.array(data)
+    # pred = model.predict(data)
+    # return json.dumps(pred)
